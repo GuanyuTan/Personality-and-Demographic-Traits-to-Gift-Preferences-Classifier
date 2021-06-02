6b064be3dbe04ae5a71aff2a581e03562060cd03
jaws <jaws@pop-os.localdomain>
2021-06-02 10:21:24 +0800

    added further EDA and data preprocessing

--- utils.py
+++ utils.py
@@ -44,7 +44,6 @@ def check_df(df, show_all=False, show_unique=False):
 
 def data_preprocessing(df):
     df['Birthday'] = df['Birthday'].apply(lambda _: datetime.strptime(_,"%Y-%m-%d"))
-
     
     #create necessary columns
     df['Timestamp']= df['Timestamp'].astype("datetime64")
@@ -75,7 +74,9 @@ def data_preprocessing(df):
 
     #remove weird col
     new_df.drop(['g_A smile HAHAHAHA'], axis=1, inplace=True)
-
+    new_df.drop(['g_I would choose all'], axis=1, inplace=True)
+    new_df.drop(['g_Not necessarily'], axis=1, inplace=True)
+    
     #remove NAN in movie - NAN means they don't like all movie 
     for col in new_df.columns:
       if col.startswith('m_'):
@@ -83,16 +84,73 @@ def data_preprocessing(df):
         
     return new_df
 
-def get_dataset():
+def get_dataset(filter=False):
     # returns data tuple with X_train, X_test, y_train, y_test
     df = pd.read_csv("data/Demographic to Gift Preference Survey.csv")
-    new_df = data_preprocessing(df)
-    y = new_df.iloc[:, 20:32]
-    X = new_df.drop(y.columns, axis=1)
+    if filter:
+        new_df = data_preprocessing_X(df)
+        y = new_df.iloc[:, 21:33]
+        X = new_df.drop(y.columns, axis=1)
+    else:
+        new_df = data_preprocessing(df)
+        y = new_df.iloc[:, 21:33]
+        X = new_df.drop(y.columns, axis=1)
     # Using a MinMaxScaler
     scaler = MinMaxScaler()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=16)
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
     return X_train, X_test, y_train, y_test
+
+def data_preprocessing_X(df):
+    df['Birthday'] = df['Birthday'].apply(lambda _: datetime.strptime(_,"%Y-%m-%d"))
     
+    
+    #create necessary columns
+    df['Timestamp']= df['Timestamp'].astype("datetime64")
+    df['Age'] = df['Timestamp'].dt.year - df['Birthday'].apply(lambda x: x.year)
+
+    # Utilizing Relationship Period
+    # Will not be using Relationship Period due to low correlation with output     
+#     df['Relationship Period (Starting Date)'] = df['Relationship Period (Starting Date)'].astype("datetime64")
+#     df['Relationship Period'] = df['Timestamp'].dt.to_pydatetime() - df['Relationship Period (Starting Date)'].dt.to_pydatetime()
+#     df['Relationship Period'] = df['Relationship Period'].dt.days
+#     # Replacing Nan with 0 
+#     df['Relationship Period'] = df['Relationship Period'].replace(np.nan,0)
+    
+    #remove outliers
+    df['Age'] = df['Age'].apply(lambda x: x if (x >18 and x <60) else df['Age'].median()) #use median to impute age
+    
+    
+    #remove unnecessary columns
+    df = df.drop(['Timestamp', 'Birthday', 'Relationship Period (Starting Date)'], axis=1)
+
+    #rename for readability
+    df.rename(columns={'Hobby (Select 3)': 'Hobby', 'Gift Preferences (Choose 3)': 'Gift Preferences'}, inplace=True)
+
+    #Handle single feature with multiple values through one-hot encoding
+    multi_val_cols = ['Hobby', 'Movie Preferences', 'Gift Preferences']
+    
+    #Body State removed due to imbalanced variable
+    df.drop(["Body State"], axis=1, inplace=True)
+    new_df = multi_cat_val_handler(df, multi_val_cols)
+    new_df = pd.get_dummies(new_df, drop_first=True)
+    
+    #remove weird col
+    new_df.drop(['g_A smile HAHAHAHA'], axis=1, inplace=True)
+    new_df.drop(['g_I would choose all'], axis=1, inplace=True)
+    new_df.drop(['g_Not necessarily'], axis=1, inplace=True)
+    
+    # Through the correlation matrix, we try to eliminate the variables with lesser correlation values (value<|0.15|).
+    # h_Collecting eliminated due to low frequency.
+    # We eliminate Body_State, Relationship Status due to the highly imbalanced data. 
+    new_df.drop(["Relationship Status_Married"], axis=1, inplace=True)
+    # We eliminate Relationship Period, m_Drama, How_do_you_get_your_energy_I due to low correlation values.
+    new_df.drop(["h_Collecting (stamps, coins)"], axis=1, inplace=True)
+    new_df.drop(["m_Drama","How do you get your energy?_I"],axis=1, inplace=True)
+    #remove NAN in movie - NAN means they don't like all movie 
+    for col in new_df.columns:
+      if col.startswith('m_'):
+        new_df[col] = new_df[col].fillna(0)
+        
+    return new_df
